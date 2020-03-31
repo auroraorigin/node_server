f34824eab39cb796e44bbed8aab4d24e066f7099
auroraorigin <226608960@qq.com>
2020-03-31 21:49:19 +0800

    页面管理

--- model/admin/ad.js
+++ model/admin/ad.js
@@ -8,7 +8,7 @@ const adSchema = new mongoose.Schema({
         required: true
     },
     notice: {
-        type: [String],
+        type: [{name:String}],
         required: true
     },
     floor: {
@@ -32,7 +32,7 @@ const Ad = mongoose.model('ad', adSchema);
 
 // Ad.create({
 //     swiper:["http://127.0.0.1:8888/uploads/upload_4eaceda3e10c674b195969820de17af1.jpg","http://127.0.0.1:8888/uploads/upload_d92409b79c53859ed0013a788d629926.jpg"],
-//     notice:["测试公告","公告示例","公告显示"],
+//     notice:[{name:"测试公告"},{name:"公告示例"},{name:"公告显示"}],
 //     floor:[{url:"http://127.0.0.1:8888/uploads/upload_2766c081b3cc6a3a33b5621da227059a.jpg",goods:['5e75dc502fce220784b68850','5e7835f6672c4c1eb4c3937c']},{url:"http://127.0.0.1:8888/uploads/upload_2766c081b3cc6a3a33b5621da227059a.jpg",goods:['5e75dc502fce220784b68850','5e7835f6672c4c1eb4c3937c']}],
 //     cate:[{name:"推荐",goods:['5e75dc502fce220784b68850','5e7835f6672c4c1eb4c3937c']}]
 // })
--- route/admin/ads.js
+++ route/admin/ads.js
@@ -11,28 +11,54 @@ const {
     Goods,
 } = require('../../model/admin/goods')
 
+// 获取推广信息列表
 router.get('/', async (req, res) => {
     var data = await Ad.findOne().lean()
     var temp = []
     for (let i = 0; i < data.floor.length; i++) {
-            temp=await Goods.find({
-                _id: {
-                    $in: data.floor[i].goods
-                }
-            })
-            data.floor[i].goods=temp
+        temp = await Goods.find({
+            _id: {
+                $in: data.floor[i].goods
+            }
+        }).populate('categories', 'name')
+        data.floor[i].goods = temp
     }
 
     for (let i = 0; i < data.cate.length; i++) {
-            temp=await Goods.find({
-                _id: {
-                    $in: data.cate[i].goods
-                }
-            })
-            data.cate[i].goods=temp
+        temp = await Goods.find({
+            _id: {
+                $in: data.cate[i].goods
+            }
+        }).populate('categories', 'name')
+        data.cate[i].goods = temp
     }
 
     res.sendResult(data, 200, '获取成功')
+})
+
+router.put('/swiper', async (req, res) => {
+    const {
+        swiper
+    } = req.body
+    var nswiper = []
+
+    for (let i = 0; i < swiper.length; i++) {
+        nswiper.push(swiper[i].url)
+    }
+
+    await Ad.updateOne({}, {
+        swiper: nswiper
+    })
+    res.sendResult(null, 200, '修改成功')
+})
+
+router.put('/notice', async (req, res) => {
+    
+    await Ad.updateOne({}, {
+        notice: req.body.notice
+    })
+
+    res.sendResult(null, 200, '修改成功')
 })
 
 // 将路由对象作为模块成员进行导出
