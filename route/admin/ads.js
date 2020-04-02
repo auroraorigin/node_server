1740cf7ee7e13043bfc7fba3b5f183affb691a99
auroraorigin <226608960@qq.com>
2020-04-02 10:25:07 +0800

    完成页面管理功能

--- route/admin/ads.js
+++ route/admin/ads.js
@@ -10,8 +10,10 @@ const {
 const {
     Goods,
 } = require('../../model/admin/goods')
+// 引入分类集合
+const Category = require('../../model/admin/category')
 
-// 获取推广信息列表
+// 获取页面信息
 router.get('/', async (req, res) => {
     var data = await Ad.findOne().lean()
     var temp = []
@@ -36,6 +38,7 @@ router.get('/', async (req, res) => {
     res.sendResult(data, 200, '获取成功')
 })
 
+// 修改首页轮播图
 router.put('/swiper', async (req, res) => {
     const {
         swiper
@@ -52,12 +55,92 @@ router.put('/swiper', async (req, res) => {
     res.sendResult(null, 200, '修改成功')
 })
 
+// 修改公告
 router.put('/notice', async (req, res) => {
-    
+
     await Ad.updateOne({}, {
         notice: req.body.notice
     })
 
+    res.sendResult(null, 200, '修改成功')
+})
+
+// 获取商品信息
+router.get('/goods', async (req, res) => {
+    let data = await Category.find({}, {
+        __v: 0,
+        url: 0
+    }).lean()
+    json = JSON.parse(JSON.stringify(data).replace(/_id/g, "value").replace(/name/g, "label"));
+
+    for (let i = 0; i < json.length; i++) {
+        json[i].children = []
+        let goods = await Goods.find({
+            categories: json[i].value
+        }).populate('categories', 'name')
+
+        for (let j = 0; j < goods.length; j++) {
+            temp = {
+                value: {},
+                label: ''
+            }
+            temp.value = goods[j]
+            temp.label = goods[j].name
+            json[i].children.push(temp)
+        }
+    }
+    res.sendResult(json, 200, '获取商品列表成功')
+})
+
+// 修改首页楼层
+router.put('/floor', async (req, res) => {
+    const {
+        floor
+    } = req.body
+    let nfloor = []
+    for (let i = 0; i < floor.length; i++) {
+        let temp = {
+            url: '',
+            goods: []
+        }
+        temp.url = floor[i].url
+        for (let j = 0; j < floor[i].goods.length; j++) {
+            temp.goods.push(floor[i].goods[j]._id)
+        }
+
+        if (temp.url)
+            nfloor.push(temp)
+    }
+
+    await Ad.updateOne({}, {
+        floor: nfloor
+    })
+    res.sendResult(null, 200, '修改成功')
+})
+
+// 修改分类楼层
+router.put('/cate', async (req, res) => {
+    const {
+        cate
+    } = req.body
+    let ncate = []
+    for (let i = 0; i < cate.length; i++) {
+        let temp = {
+            name: '',
+            goods: []
+        }
+        temp.name = cate[i].name
+        for (let j = 0; j < cate[i].goods.length; j++) {
+            temp.goods.push(cate[i].goods[j]._id)
+        }
+
+        if (temp.name && temp.goods.length)
+            ncate.push(temp)
+    }
+
+    await Ad.updateOne({}, {
+        cate: ncate
+    })
     res.sendResult(null, 200, '修改成功')
 })
 
