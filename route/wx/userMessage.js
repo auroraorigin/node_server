8c5af6a42ce5d72425074fb55949c5c9eafc8d46
auroraorigin <226608960@qq.com>
2020-04-29 16:31:19 +0800

    优化

--- route/wx/userMessage.js
+++ route/wx/userMessage.js
@@ -26,7 +26,7 @@ module.exports.getMessage = async (req, res) => {
     jwt.verify(token, secretOrKey, (err, decode) => {
         if (err) {
             return res.json({
-                "status":"error"
+                "status": "error"
             })
         } else {
             // token验证通过，放行
@@ -56,7 +56,7 @@ module.exports.getMessage = async (req, res) => {
                 }
                 else {
                     return res.json({
-                        "status":"error"
+                        "status": "error"
                     })
                 }
             })
@@ -72,7 +72,7 @@ module.exports.messageUpdata = async (req, res) => {
     jwt.verify(token, secretOrKey, (err, decode) => {
         if (err) {
             return res.json({
-                "status":"error"
+                "status": "error"
             })
         } else {
             // token验证通过，放行
@@ -87,14 +87,14 @@ module.exports.messageUpdata = async (req, res) => {
                         wxNumber: req.body.wxNumber,
                         detailAddress: req.body.detailAddress
                     };
-                    User.updateOne({ openid: data.openid }, message).then(result => {});
+                    User.updateOne({ openid: data.openid }, message).then(result => { });
                     return res.json({
                         "status": "ok",
                     });
                 }
                 else {
                     return res.json({
-                        "status":"error"
+                        "status": "error"
                     })
                 }
             })
@@ -107,12 +107,36 @@ module.exports.getCouponList = async (req, res) => {
     // 判断token是否有效
     const token = req.get("Authorization");
     const secretOrKey = keys.secretOrKey;
-    jwt.verify(token, secretOrKey, (err, decode) => {
+    jwt.verify(token, secretOrKey, async (err, decode) => {
         if (err) {
             return res.json({
-                "status":"error"
+                "status": "error"
             })
         } else {
+            var timestamp = Date.parse(new Date());
+            var date = new Date(timestamp);
+            //获取年份  
+            var Y = date.getFullYear();
+            //获取月份  
+            var M = Number((date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1));
+            //获取当日日期 
+            var D = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+
+            var coupon = await Coupon.find({ openid: decode.openid, state: "可用" });
+            coupon.forEach(function (v, i) {
+                var arr = v.effective[1].split(".");
+                if (Number(arr[0]) < Y) {//如果优惠卷过期年份小于当前年份，表示已过期
+                    v.state = "不可用"
+                    Coupon.updateOne({ _id: v._id }, { state: v.state },(err,data)=>{});
+                } else if (Number(arr[0]) == Y && Number(arr[1]) < M) {//如果优惠卷过期年份等于当前年份但月份小于当前月份，表示已过期
+                    v.state = "不可用"
+                    Coupon.updateOne({ _id: v._id }, { state: v.state },(err,data)=>{});
+                } else if (Number(arr[0]) == Y && Number(arr[1]) == M && Number(arr[2]) < D) {
+                    //如果优惠卷过期年月等于当前年月但日小于当前日，表示已过期
+                    v.state = "不可用"
+                    Coupon.updateOne({ _id: v._id }, { state: v.state },(err,data)=>{});
+                }
+            })
             // token验证通过，放行
             if (req.body.state == 1) {//表示要获取可用优惠卷列表
                 var state = "可用"
@@ -138,36 +162,36 @@ module.exports.cheapState = async (req, res) => {
     jwt.verify(token, secretOrKey, async (err, decode) => {
         if (err) {
             return res.json({
-                "status":"error"
+                "status": "error"
             })
         } else {
-            let coupon = await Coupon.find({ openid: decode.openid },"name money state effective couponCenterId");
+            let coupon = await Coupon.find({ openid: decode.openid }, "name money state effective couponCenterId");
             //可使用优惠卷数组
             let canUseCoupon = [];
             //将满足满减的优惠卷数组进行拼接
-            if (coupon.length!=0) {
+            if (coupon.length != 0) {
                 for (var i = 0; i < coupon.length; i++) {
                     if (Number(coupon[i].money[0]) <= Number(req.body.totalMoney))
-                        canUseCoupon=canUseCoupon.concat(coupon[i])
+                        canUseCoupon = canUseCoupon.concat(coupon[i])
                 }
                 //如果可用优惠卷为true
-                if(canUseCoupon.length!=0){
+                if (canUseCoupon.length != 0) {
                     return res.json({
-                        "status":"ok",
-                        "canUseCoupon":canUseCoupon,
-                        "state":"有可用优惠卷"
+                        "status": "ok",
+                        "canUseCoupon": canUseCoupon,
+                        "state": "有可用优惠卷"
                     })
-                }else{
+                } else {
                     return res.json({
-                        "status":"ok",
-                        "state":"无可用优惠卷"
+                        "status": "ok",
+                        "state": "无可用优惠卷"
                     })
                 }
-            }else{
+            } else {
                 return res.json({
-                    "status":"error",
-                    "message":"用户未拥有任何优惠卷",
-                    "state":"无可用优惠卷"
+                    "status": "error",
+                    "message": "用户未拥有任何优惠卷",
+                    "state": "无可用优惠卷"
                 })
             }
         }
