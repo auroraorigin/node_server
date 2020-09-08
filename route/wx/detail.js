bb883a1881fa9031ea7447104c0685abef634a95
auroraorigin <226608960@qq.com>
2020-09-08 14:00:37 +0800

    格式化

--- route/wx/detail.js
+++ route/wx/detail.js
@@ -7,12 +7,11 @@ const {
 } = require('../../model/admin/ad')
 
 module.exports = async (req, res) => {
-    let { _id }= req.query
+    let { _id } = req.query
     let temp = JSON.parse(_id)
-    
-    let goods = await Goods.findOne({_id:temp._id},{categories:0,state:0,__v:0})
+    let goods = await Goods.findOne({ _id: temp._id }, { categories: 0, state: 0, __v: 0 })
     let ad = await Ad.findOne({})
     let detail = ad.detail
     let discount = ad.discount
-    res.json({"data":{goods,detail,discount},"status":200})
+    res.json({ "data": { goods, detail, discount }, "status": 200 })
 }
